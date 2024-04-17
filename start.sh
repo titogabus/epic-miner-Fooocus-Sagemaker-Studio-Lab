e6a3f9b396d1834178e9aa1c1609be090936d0d2
titogabus <77083733+titogabus@users.noreply.github.com>
2024-04-17 12:30:52 +0700

    start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 if [ ! -d "Fooocus" ]
 then
-  git clone https://github.com/lllyasviel/Fooocus.git
-  #git clone --depth 1 --branch V2 https://github.com/lllyasviel/Fooocus.git
+  git clone https://github.com/titogabus/Fooocus.git
+  #git clone --depth 1 --branch V2 https://github.com/titogabus/Fooocus.git
   # Create the config file pointing the checkpoints to checkpoints-real-folder
 fi
 cd Fooocus
